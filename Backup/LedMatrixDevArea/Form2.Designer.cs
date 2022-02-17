099e5b0dc8e32246cc8ef435856887a949b09b05
lima1977 <marcosferreira66@hotmail.com>
2022-02-17 12:28:41 -0300

    Form2

--- Backup/LedMatrixDevArea/Form2.Designer.cs
+++ Backup/LedMatrixDevArea/Form2.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace LedMatrixDevArea
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -29,17 +29,10 @@ namespace LedMatrixDevArea
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(977, 522);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form2";
         }
 
         #endregion
